03993cc93a7dce84ea7f7170008231ae4c69851b
Dissarion <dissarion.dev@gmail.com>
2021-02-11 23:34:58 +0300

    feat: trim symbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0 || string === '') {
+    return '';
+  }
 
+  let trimmedString = string[0];
+  let count = 1;
+
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === trimmedString[trimmedString.length - 1]) {
+      if (count === size) {
+        continue;
+      } else {
+        trimmedString += string[i];
+        count += 1;
+      }
+    } else {
+      count = 1;
+      trimmedString += string[i];
+    }
+  }
+
+  return trimmedString;
 }
+
